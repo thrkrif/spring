d039ec5d5566f0e179375217b0bd8953bd2d07d9
thrkrif <103479629+thrkrif@users.noreply.github.com>
2025-02-17 19:28:34 +0900

    25/02/17

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -15,19 +15,47 @@ public class HelloController {
         return "hello";
     }
     // mvc 방식
+    // 컨트롤러를 찾아서 viewResolver에게 던진다
     @GetMapping("hello-mvc")
     public String helloMvc(@RequestParam("name") String name, Model model){
         model.addAttribute("name", name);
         return "hello-template";
     }
 
-    // API 방식
+    // API 방식 : resources에 html 파일을 생성하지 않아도 된다.
     // 위의 mvc 방식과 차이 없어보이지만 페이지 소스를 보면 웹 브라우저 화면에 작성한
     // 글만 보인다.
     @GetMapping("hello-string")
     @ResponseBody   // 넣어줘야함. http의 body 부분에 return 값을 직접 넣어주겠다.
+    // ResponseBody 어노테이션이 있으면 지금 return 처럼 string만 전달하는 경우
+    // viewResolver에게 던지지 않고 http 응답에 바로 넣어 전달한다.
     public String helloString(@RequestParam("name") String name){
-        return "hello " + name; // "hello-string"
+        return "hello " + name; // "hello-string" , 실제로 이렇게 쓰는 일은 없다.
+    }
+
+    // 진짜 API를 보여주마.
+    @GetMapping("hello-api")
+    @ResponseBody // 이 어노테이션이 있으면 viewResolver에게 던지지 않는다.
+    // 위와 다른 점은 return 값이 객체라는 점
+    // 객체인 경우 default : json 방식으로 데이터를 만들어서 http 응답에 반환한다.
+    // 더 디테일 한 건 뒤에 설명하겠다.
+    // 객체를 json으로 바꿔주는 대표적 라이브러리 : Jackson
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 
 }
---
+++ src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
@@ -0,0 +1,36 @@
+package hello.hello_spring.repository;
+
+import hello.hello_spring.domain.Member;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findId(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return null;
+    }
+}
